f097fd7a962c697107b0a5029dd5c040ef836d6a
Кирилл Ламин <lamalamovic589@gmail.com>
2026-09-25 16:03:31 +0300

    Обновил missions.py

--- missions.py
+++ missions.py
@@ -2,7 +2,7 @@
 
 MISSIONS = [
     {"name": "Mars 2020", "year": 2020, "direction": "Марс"},
-    {"name": "Artemis I", "year": 2022, "direction": "Луна"},
+    {"name": "Artemis 1", "year": 2022, "direction": "Луна"},
     {"name": "Voyager 1", "year": 1977, "direction": "Дальний космос"},
     {"name": "Chandrayaan-3", "year": 2023, "direction": "Луна"},
 ]
